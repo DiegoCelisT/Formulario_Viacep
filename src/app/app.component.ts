46c943421e5e15f04c7c524647dd8a458d384e57
Diego Celis <diegofercelis@gmail.com>
2021-03-22 01:23:04 -0300

    Feito o tratamento de erros no formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { EnderecoService } from './endereco.service';
 export class AppComponent {
   
   endereco: any = {};
-  tem_endereco: boolean = false;  //new
+  tem_endereco: boolean = false;
 
   constructor (private Service_End: EnderecoService){}
 
@@ -21,13 +21,20 @@ export class AppComponent {
     else{
       // console.log (formulario.form.value);
 
-      this.Service_End.getEndereco(formulario.form.value.zipcode).subscribe(endereco => this.endereco = endereco);
+      //eliminar os trazos caso o usuario escreva os CEPs dessa maneira:
+      // let zipcode = formulario.form.value.zipcode.replace ('-','');
 
-      this.tem_endereco = true; //new
+      this.Service_End.getEndereco(formulario.form.value.zipcode)
+        .subscribe(endereco => {
+          this.endereco = endereco;
+          this.tem_endereco = true;
+          }, //se liga na virgula e que tudo está dentro do parentesis do subscribe
+        () => alert  ('CEP incorreto, corrija por favor')// Os parentesis podem se substituir por uma variavel chamada por exemplo "ermac"
+      )
     }
   }
 
-  //segundo botão (new):
+  //segundo botão:
   limpar (){
     this.endereco = [];
     this.tem_endereco = false;
